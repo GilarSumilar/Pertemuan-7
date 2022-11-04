b34e2d426b18ad4641e2dc4a46cd8f5efe707edf
GilarSumilar <gilarz2100@gmail.com>
2022-11-04 13:13:49 +0700

    Ganti nama

---
+++ Tugas Struktur kondisi/latihan1.py
@@ -0,0 +1,12 @@
+bil1 = int (input("Masukan bilangan : "))
+bil2 = int (input("Masukan bilangan : "))
+
+#Nilai terbesar
+
+if (bil1 > bil2):
+    print("Bilangan terbesar :",bil1)
+
+#Nilai terkecil
+
+if (bil1 < bil2):
+    print("Bilangan terbesar :",bil2)
